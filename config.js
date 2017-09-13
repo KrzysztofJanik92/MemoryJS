f7b014adc7106732197d339445f46016cd98e28d
KrzysztofJanik92 <krzysztof.janik@vp.pl>
2017-09-13 16:16:37 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,20 @@
+var config = {
+    easy: {
+        gameLev: 0,
+        tiles: 10,
+        textLevel: ' EASY',
+        styles: 'css/memory-easy.css',
+    },
+    hard: {
+        gameLev: 1,
+        tiles: 20,
+        textLevel: ' HARD',
+        styles: 'css/memory-hard.css'
+    },
+    extreme: {
+        gameLev: 2,
+        tiles: 20,
+        textLevel: ' EXTREME',
+        styles: 'css/memory-extreme.css'
+    }
+}
